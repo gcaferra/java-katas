3196adb1739027808e91c65dcec5acde7c456839
Giuseppe Caferra <g.caferra@icloud.com>
2023-11-29 11:55:43 +0100

    Code cleanup

--- bowling-game/src/test/java/BowlingGameTest.java
+++ bowling-game/src/test/java/BowlingGameTest.java
@@ -3,23 +3,23 @@ import junit.framework.TestCase;
 public class BowlingGameTest extends TestCase {
     private Game g;
     @Override
-    protected void setUp() throws Exception {
+    protected void setUp() {
         g = new Game();
     }
 
-    public void  testGutterGame() throws Exception {
+    public void  testGutterGame() {
         rollMany(0, 20);
 
         assertEquals(0, g.score());
     }
 
-    public void testAllOne() throws Exception {
+    public void testAllOne() {
         rollMany(1, 20);
 
         assertEquals(20, g.score());
     }
 
-    public void testOneSpare() throws Exception {
+    public void testOneSpare() {
         g.roll(5);
         g.roll(5);
         g.roll(3);
@@ -28,7 +28,7 @@ public class BowlingGameTest extends TestCase {
         assertEquals(16, g.score());
     }
 
-    public void testOneStrike() throws Exception {
+    public void testOneStrike() {
         g.roll(10);
         g.roll(3);
         g.roll(4);
@@ -37,7 +37,7 @@ public class BowlingGameTest extends TestCase {
         assertEquals(24, g.score());
     }
 
-    public void testPerfectGame() throws Exception {
+    public void testPerfectGame() {
         rollMany(10,12);
         assertEquals(300, g.score());
     }
