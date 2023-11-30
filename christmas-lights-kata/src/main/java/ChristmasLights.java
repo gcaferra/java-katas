cfde4c47a39c207186cc1059bef382d0a7247df7
Giuseppe Caferra <g.caferra@icloud.com>
2023-11-30 17:57:41 +0100

    added turnOff and toggle methods

--- christmas-lights-kata/src/main/java/ChristmasLights.java
+++ christmas-lights-kata/src/main/java/ChristmasLights.java
@@ -10,4 +10,14 @@ public class ChristmasLights {
             for(int row = y1; row <= y2; row++)
                 grid[col][row] = 1;
     }
+    public void turnOff(int x1, int y1, int x2, int y2) {
+        for(int col = x1; col <= x2; col++)
+            for(int row = y1; row <= y2; row++)
+                grid[col][row] = 0;
+    }
+    public void toggle(int x1, int y1, int x2, int y2) {
+        for(int col = x1; col <= x2; col++)
+            for(int row = y1; row <= y2; row++)
+                grid[col][row] = grid[col][row] == 0 ? 1: 0;
+    }
 }
--- christmas-lights-kata/src/test/java/ChristmasLightsTest.java
+++ christmas-lights-kata/src/test/java/ChristmasLightsTest.java
@@ -27,4 +27,59 @@ public class ChristmasLightsTest {
                 assertEquals(1, cl.getStatus(x,y), String.format("{%s} x {%s}", x, y));
 
     }
+
+    @Test
+    public void theGridCanBeFullyTurnedOffOnceTurnedOn() {
+
+        cl.turnOn(0,0, 999,999);
+        cl.turnOff(0,0, 999,999);
+
+        for (int x = 0; x < 1000; x++)
+            for(int y = 0; y < 1000; y++)
+                assertEquals(0, cl.getStatus(x,y), String.format("{%s} x {%s}", x, y));
+
+    }
+    @Test
+    public void turningOnOnlyTheFirstLine() {
+
+        cl.turnOn(0,0, 999,0);
+
+        for (int x1 = 0; x1 < 1000; x1++)
+            assertEquals(1, cl.getStatus(x1,0), String.format("x1:%s x %s", x1, 0));
+
+        for (int x = 1; x < 1000; x++)
+            for(int y = 1; y < 1000; y++)
+                assertEquals(0, cl.getStatus(x,y), String.format("x:%s x y:%s", x, y));
+
+    }
+
+    @Test
+    public void toggleChangeTheLightStatus() {
+
+        cl.turnOn(0,0, 999,999);
+
+        cl.toggle(0,0,999,999);
+
+        for (int x = 1; x < 1000; x++)
+            for(int y = 1; y < 1000; y++)
+                assertEquals(0, cl.getStatus(x,y), String.format("x:%s x y:%s", x, y));
+    }
+
+    @Test
+    public void overAllTest() { //Readme Example
+
+        cl.turnOn(0,0, 999,999);
+
+        cl.toggle(0,0,999,0);
+
+        cl.turnOff(499,499, 500,500);
+
+        int lightsCount = 0;
+
+        for (int x = 1; x < 1000; x++)
+            for(int y = 1; y < 1000; y++)
+                 lightsCount += cl.getStatus(x,y);
+
+        assertEquals(997997, lightsCount);
+    }
 }
